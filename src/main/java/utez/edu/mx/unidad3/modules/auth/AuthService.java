52875334cab3b412b117a74061c402f625cac6c6
JEMB1432 <20213tn094@utez.edu.mx>
2025-07-07 18:53:57 -0600

    [ADD] Modificacion

---
+++ src/main/java/utez/edu/mx/unidad3/modules/auth/AuthService.java
@@ -0,0 +1,45 @@
+package utez.edu.mx.unidad3.modules.auth;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import utez.edu.mx.unidad3.modules.auth.dto.LoginRequestDTO;
+import utez.edu.mx.unidad3.modules.user.User;
+import utez.edu.mx.unidad3.modules.user.UserRepository;
+import utez.edu.mx.unidad3.security.jwt.JWTUtils;
+import utez.edu.mx.unidad3.security.jwt.UDService;
+import utez.edu.mx.unidad3.utils.APIResponse;
+
+@Service
+public class AuthService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private JWTUtils jwtUtils;
+
+    @Autowired
+    private UDService udService;
+
+    @Transactional(readOnly = true)
+    public APIResponse doLogin(LoginRequestDTO payload){
+        try {
+            User found = userRepository.findByUsernameAndPassword(
+                    payload.getUsername(),
+                    payload.getPassword()
+            ).orElse(null);
+            if (found == null) {
+                return new APIResponse("Usuario no encontrado", false, HttpStatus.NOT_FOUND);
+            }
+            UserDetails userDetails = udService.loadUserByUsername(found.getUsername());
+            String token = jwtUtils.generateToken(userDetails);
+            return new APIResponse("Inicio de sesión exitoso", token,false, HttpStatus.OK);
+
+        }catch (Exception e){
+            e.printStackTrace();
+            return new APIResponse("Error al iniciar sesión", false, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
--- src/main/java/utez/edu/mx/unidad3/security/jwt/JWTUtils.java
+++ src/main/java/utez/edu/mx/unidad3/security/jwt/JWTUtils.java
@@ -3,10 +3,14 @@ package utez.edu.mx.unidad3.security.jwt;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.function.Function;
 
 @Service
@@ -30,5 +34,38 @@ public class JWTUtils {
     public Claims extractAllClaims(String token) {
         return Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token).getBody();
     }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    //Consume la funcion de arriba adicional a que pregunta que el usuario del token
+    // coincida con el usuario que lo usa
+    //UserDetails = pasaporte
+    public Boolean validateToken(String token, UserDetails userDetails){
+        final String USERNAME = extractUsername(token);
+        return (USERNAME.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    //Crea el token a raiz de la informacion del usuario y el token
+    private String createToken(Map<String, Object> claims, String subject){
+        return Jwts.builder()
+                .setClaims(claims).setSubject(subject) // informacion del usuario
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(
+                        System.currentTimeMillis() + 1000 * 60 * 60 * 10
+                ))
+                .signWith(SignatureAlgorithm.HS256,SECRET_KEY)
+                .compact();
+         //Construir un token
+    }
+
+    public String generateToken(UserDetails userDetails){
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, userDetails.getUsername());
+    }
+    //Consume la funcion de crear apra solamente exportar el token
+
+
 
 }
